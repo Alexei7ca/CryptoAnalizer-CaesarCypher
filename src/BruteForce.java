211ee03ea9822b241f05140b93095c918a651ca8
Alexei7ca <97787086+Alexei7ca@users.noreply.github.com>
2022-01-25 16:31:51 +0200

    cleaned up the class, got rid of unused code. Started writing the filter isMessageValid

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,115 +1,22 @@
 public class BruteForce {
 
 
-    static class allLettersAndSymbols {
-
-        StringBuilder builder = new StringBuilder();
-        String allChar = "";{
-            for (int i = 32; i < 1103; i++) {
-                char a = (char) i;
-                builder.append(a);
-            }
-            allChar = builder.toString();
-        }
-
-
-        int indexOfChar(char c)
-        {
-            for(int i=0;i< allChar.length();i++)
-            {
-                if(allChar.charAt(i)==c)
-                    return i;
-            }
-            return -1;
-        }
-
-        char charAtIndex(int pos)
-        {
-            return allChar.charAt(pos);
-        }
-    }
-        static allLettersAndSymbols b= new allLettersAndSymbols();
-
-        static void bruteForceDecoder(String cipherText)
-        {
-            cipherText=cipherText.toUpperCase();
-
-            for(int k=0;k< 1071;k++)
-            {
-                String decryptedText="";
-                int key=k;
-                for(int i=0;i< cipherText.length();i++)
-                {
-                    int index= b.indexOfChar(cipherText.charAt(i));
-
-                    if(index==-1)
-                    {
-                        decryptedText+=cipherText.charAt(i);
-                        continue;
-                    }
-                    if((index-key)>=0)
-                    {
-                        decryptedText+= b.charAtIndex(index-key);
-                    }
-                    else
-                    {
-                        decryptedText+= b.charAtIndex((index-key)+1071);
-                    }
-                }
-
+    static void bruteForceDecoder(String cipherText) {
+        for (int key = 0; key < 1071; key++) {
+            String decryptedText = CaesarCipher.decrypt(cipherText, key);
+            if (isMessageValid(decryptedText))
                 System.out.println("Decrypted Text Using key"+key+":"+decryptedText);
-            }
         }
 
-    // this is the one for russian symbols////////// not needed anymore
-
-//    static class russianLetters {
-//        String allChar="АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-//        int indexOfChar(char c)
-//        {
-//            for(int i=0;i< allChar.length();i++)
-//            {
-//                if(allChar.charAt(i)==c)
-//                    return i;
-//            }
-//            return -1;
-//        }
-//
-//        char charAtIndex(int pos)
-//        {
-//            return allChar.charAt(pos);
-//        }
-//    }
-//    static russianLetters a = new russianLetters();
-//
-//    static void bruteForceRussian(String cipherText)
-//    {
-//        cipherText=cipherText.toUpperCase();
-//
-//        for(int k=0;k< 32;k++)
-//        {
-//            String decryptedText="";
-//            int key=k;
-//            for(int i=0;i< cipherText.length();i++)
-//            {
-//                int index= a.indexOfChar(cipherText.charAt(i));
-//
-//                if(index==-1)
-//                {
-//                    decryptedText+=cipherText.charAt(i);
-//                    continue;
-//                }
-//                if((index-key)>=0)
-//                {
-//                    decryptedText+= a.charAtIndex(index-key);
-//                }
-//                else
-//                {
-//                    decryptedText+= a.charAtIndex((index-key)+32);
-//                }
-//            }
-//
-//            System.out.println("Decrypted Text Using key"+key+":"+decryptedText);
-//        }
-//    }
     }
+    public static boolean isLatinLetter(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+
+    private static boolean isMessageValid(String decryptedText) { //does not work with spaces or symbols or numbers or russian
+        for (char c : decryptedText.toCharArray())
+            if (!isLatinLetter(c))
+                return false;
+        return true;
+    }
+}
